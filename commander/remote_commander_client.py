260ac572b865f0348da85cacd5fa87f9fad75b5a
czajowaty <adam.czajkowski@gmail.com>
2020-11-04 16:04:47 -0600

    Remote commander client can take command to send from arguments.

--- commander/remote_commander_client.py
+++ commander/remote_commander_client.py
@@ -3,14 +3,18 @@ import socket
 import sys
 
 
-async def main_loop(port):
+def send_command(port: int, command: str):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.sendto(command.encode(), ('127.0.0.1', port))
+    print(f"Sent command: '{command}'.")
+
+
+async def main_loop(port):
     while True:
         command = input("Enter command: ")
         if command.strip().lower() == 'exit':
             return
-        s.sendto(command.encode(), ('127.0.0.1', port))
-        print(f"Sent command: '{command}'.")
+        send_command(port, command)
 
 
 def main():
@@ -23,8 +27,11 @@ def main():
     except ValueError as exc:
         print(f"Invalid port - {exc}.")
         return
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main_loop(port))
+    if len(args) == 2:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main_loop(port))
+    else:
+        send_command(port, ' '.join(args[2:]))
 
 
 if __name__ == '__main__':
